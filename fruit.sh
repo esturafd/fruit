3028ac98819de54ceda280bf9a4624037f0cbffb
Estuardo Ramos <estuardo.ramos@esturafd.me>
2021-01-15 21:54:25 -0600

    🎉 script fruit is created

---
+++ fruit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+# Variables:
+# FRUIT_ACCOUNT: bitwarden account email
+# FRUIT_CONFIG: ssh configuration file, if it doesn't exits, ~/.ssh/config will be used
+
+
+if [ -z "$FRUIT_CONFIG" ]; then
+    CONFIG_FILE="-f $FRUIT_CONFIG"
+fi
+
+export BW_SESSION=$(bw login $FRUIT_ACCOUNT --raw)
+PASSWORD=$(bw get password ssh:$1)
+bw logout
+sshpass -p "$PASSWORD" ssh $CONFIG_FILE "$@"
