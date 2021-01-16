6c938c0c08876ca02267b0ec7a190f54937a26e5
Estuardo Ramos <estuardo.ramos@esturafd.me>
2021-01-15 22:52:13 -0600

    🐛 reading of configfile is fixed and logout output is removed

--- fruit.sh
+++ fruit.sh
@@ -5,11 +5,11 @@
 # FRUIT_CONFIG: ssh configuration file, if it doesn't exits, ~/.ssh/config will be used
 
 
-if [ -z "$FRUIT_CONFIG" ]; then
+if [ -n "$FRUIT_CONFIG" ]; then
     CONFIG_FILE="-F $FRUIT_CONFIG"
 fi
 
 export BW_SESSION=$(bw login $FRUIT_ACCOUNT --raw)
 PASSWORD=$(bw get password ssh:$1)
-bw logout
+bw logout > /dev/null &
 sshpass -p "$PASSWORD" ssh $CONFIG_FILE "$@"
