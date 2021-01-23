34687e3257f067aa2681351c527d584d92772c8f
Estuardo Ramos <estuardo.ramos@esturafd.me>
2021-01-22 20:52:12 -0600

    ✨ options are added
    Reader of command options other than ssh options was
    added, of which is the help and change of executor

--- fruit.sh
+++ fruit.sh
@@ -4,6 +4,43 @@
 # FRUIT_ACCOUNT: bitwarden account email
 # FRUIT_CONFIG: ssh configuration file, if it doesn't exits, ~/.ssh/config will be used
 
+EXECUTOR=ssh
+
+function help {
+    cat <<  EOM
+Manager of ssh connections by password
+
+Usege:
+   fruit [fruit-options] TARGET [ssh-options]
+
+Options:
+   -p    specific executor like ssh, sftp, etc
+   -f    sftp executor
+   -h    this help
+EOM
+    exit 0
+}
+
+if [[ $# -le 0 ]]; then
+    help
+fi
+
+while (($#)); do 
+    if echo "$1" | grep -v '^-' > /dev/null; then
+        TARGET=$1
+        shift 1
+        break
+    fi
+    case $(echo "$1" | cut -f 2 -d '-') in
+        p) EXECUTOR=$2; shift 2;;
+        f) EXECUTOR=sftp; shift 1;;
+        h) help;;
+    esac
+done
+
+if [ -z "$TARGET" ]; then
+    exit 1
+fi
 
 if [ -n "$FRUIT_CONFIG" ]; then
     CONFIG_FILE="-F $FRUIT_CONFIG"
@@ -12,4 +49,4 @@ fi
 export BW_SESSION=$(bw login $FRUIT_ACCOUNT --raw)
 PASSWORD=$(bw get password ssh:$1)
 bw logout > /dev/null &
-sshpass -p "$PASSWORD" ssh $CONFIG_FILE "$@"
+sshpass -p "$PASSWORD" $EXECUTOR $CONFIG_FILE "$@"
